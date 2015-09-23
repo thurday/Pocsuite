a336ea32beb24bb0e62c421c3068175e9f8db3e5
flsf <jianmaflsf@gmail.com>
2015-09-23 17:07:00 +0800

    initial api

---
+++ pocsuite/api/poc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2014-2015 pocsuite developers (http://sebug.net)
+See the file 'docs/COPYING' for copying permission
+"""
+
+from lib.core.poc import Output, POCBase
+from lib.core.register import registerPoc as register
+from lib.utils.require import require, require_header, require_param
---
+++ pocsuite/api/utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2014-2015 pocsuite developers (http://sebug.net)
+See the file 'docs/COPYING' for copying permission
+"""
+
+from lib.core.data import logger
+from lib.core.enums import CUSTOM_LOGGING
